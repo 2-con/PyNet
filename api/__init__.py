e921bcafe6b790b2e1557631f849fc2ff11be3d3
Aufy <72212@jisedu.or.id>
2025-10-24 20:25:09 +0700

    Migrated JAX to a different repository.

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Built-in PyNet APIs
-
-Some of these APIs are not compatible with each other, such as Synapse and Multinet.
-Synapse is designed around sequential models, while Multinet is designed around parallel models.
-"""
-
-__all__ = ["NetCore", "NetFlash", "NetLab"]
-__package__ = "pynet"
